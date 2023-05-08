dbaae3b86d7b8a4f7929ec7b4f45c3f263858b08
pavel <shoker_84@bk.ru>
2023-05-08 21:03:24 +1000

    firstcommit

---
+++ image.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+    
+import pyautogui as pg
+import time
+from sys import argv
+
+
+time.sleep(3)
+run = True
+start = time.time()
+counter = int(argv[1])
+while run:
+    if pg.locateOnScreen('res/scsh2.png') or counter == 0:
+        run = False
+    elif pg.locateOnScreen('res/scsh.png', region=(1336, 989, 136, 40), confidence=.3):
+       counter -= 1
+       pg.press('right')
+       print(f"Осталось {counter}")
+    elif pg.locateOnScreen('res/scsh3.png', region=(751, 467, 421,116), confidence=.9):
+       pg.press('esc')
+  
+    else: pg.press('left')
+print(f'Времени затрачено {int((time.time() - start)/60)}минут и {int((time.time() - start)%60)} секунд')
