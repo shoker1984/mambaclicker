ec88700f6990f1998241f167ecdef3e6e3f233a5
shoker_84@bk.ru <shoker_84@bk.ru>
2023-11-24 14:54:32 +1000

    откалибровал процент совпадения

--- image.py
+++ image.py
@@ -12,8 +12,8 @@ counter = int(argv[1])
 while run:
     if pg.locateOnScreen('res/scsh2.png') or counter == 0:
         run = False
-    elif pg.locateOnScreen('res/scsh.png', region=(1345, 965, 124, 38), confidence=.3):
-       if pg.locateOnScreen('res/scsh3.png', region=(749, 340, 430, 159), confidence=.7):
+    elif pg.locateOnScreen('res/scsh.png', region=(1345, 965, 124, 38), confidence=.2):
+       if pg.locateOnScreen('res/scsh3.png', region=(749, 340, 430, 159), confidence=.3):
              pg.press('esc')
        counter -= 1
        pg.press('right')
