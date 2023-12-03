f34b19d61226859e6ac84ed209935e2c89d020f4
shoker_84@bk.ru <shoker_84@bk.ru>
2023-12-03 12:03:06 +1000

    продолжаю оптимизацию

--- image.py
+++ image.py
@@ -12,14 +12,17 @@ counter = int(argv[1])
 while run:
     if counter == 0:
         run = False
-    elif pg.locateOnScreen('res/scsh.png', region=(1345, 965, 124, 38), confidence=.2):
-       if pg.locateOnScreen('res/scsh3.png', region=(749, 340, 430, 159), confidence=.3):
-             pg.press('esc')
+    elif pg.locateOnScreen('res/scsh.png', region=(1341, 966, 132, 36), confidence=.1):
        counter -= 1
        pg.press('right')
        print(f"Осталось {counter}")
     # elif pg.locateOnScreen('res/scsh3.png', confidence=.9):
         #    pg.press('esc')
-    else: pg.press('left')
+    elif pg.locateOnScreen('res/scsh3.png', region=(749, 340, 430, 159), confidence=.5):
+             pg.press('esc')
+       
+    else: 
+        pg.press('left')
+        counter -= 1
 print(f'Времени затрачено {int((time.time() - start)/60)} минут и {int((time.time() - start)%60)} секунд,\nвсего {int((time.time() - start))/int(argv[1])} секунд на анкету')
 
