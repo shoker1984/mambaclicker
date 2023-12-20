79a7a3dcdfaff6cf76446eb444d681f8fcb72712
shoker_84@bk.ru <shoker_84@bk.ru>
2023-12-20 18:44:07 +1000

    изменил порядок проверки

--- image.py
+++ image.py
@@ -12,14 +12,16 @@ counter = int(argv[1])
 while run:
     if counter == 0:
         run = False
+    elif pg.locateOnScreen('res/scsh3.png', region=(749, 340, 430, 159), confidence=.8):
+            pg.press('esc')
     elif pg.locateOnScreen('res/scsh.png', region=(1341, 966, 132, 36), confidence=.8):
        counter -= 1
        pg.press('right')
        print(f"Осталось {counter}")
     # elif pg.locateOnScreen('res/scsh3.png', confidence=.9):
         #    pg.press('esc')
-    elif pg.locateOnScreen('res/scsh3.png', region=(749, 340, 430, 159), confidence=.8):
-             pg.press('esc')
+    # elif pg.locateOnScreen('res/scsh3.png', region=(749, 340, 430, 159), confidence=.8):
+    #          pg.press('esc')
        
     elif pg.locateOnScreen('res/scsh4.png', region=(987, 552, 267, 146), confidence=.8) or pg.locateOnScreen('res/scsh5.png', region=(618, 289, 64, 63), confidence=.8) or pg.locateOnScreen('res/scsh6.png', region=(988, 539, 254, 93), confidence=.8): 
         pg.press('left')
