71751fc38d19c51d047582bd48fb8096ffedcbec
shoker_84@bk.ru <shoker_84@bk.ru>
2024-04-10 12:39:53 +1000

    + button run

--- image.py
+++ image.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python3
-    
+
 import pyautogui as pg
 import cv2
 import numpy as np
@@ -11,46 +11,58 @@ from sys import argv
 def main():
     root = tk.Tk()
     root.attributes('-topmost', True)
-    root.geometry('100x100+0+0')
+    root.geometry('400x400+0+0')
     root.resizable(False, False)
-    time.sleep(3)
-    start = time.time()
+    # time.sleep(3)
+    start_time = time.time()
     counter = int(argv[1])
-    run = True
-    lb = tk.Label(text=f'{counter}')
-    btn = tk.ttk.Button(text='start', command=main)
+    run = False
+    lb = (tk.Label(text=f'{counter}'))
     lb.pack(expand=True)
-    btn.pack()
-    
 
-    while run:
-        
-        scsh3 = pg.screenshot('s3.png', region=(749, 340, 430, 159))
-        scsh = pg.screenshot('s.png', region=(1341, 966, 132, 36))
-        
-        if counter == 0:
-            run = False
-
-        elif np.equal(np.array(cv2.imread('res/scsh.png', cv2.IMREAD_GRAYSCALE)), np.array(cv2.imread('s.png', cv2.IMREAD_GRAYSCALE))).all():
-            if np.equal(np.array(cv2.imread('res/scsh3.png', cv2.IMREAD_GRAYSCALE)), np.array(cv2.imread('s3.png', cv2.IMREAD_GRAYSCALE))).all():
-                pg.press('esc')
-            counter -= 1
-            lb['text'] = f'{counter}'
+    def start():
+        time.sleep(2)
+        nonlocal run
+        nonlocal counter
+        nonlocal start_time
+        run = True
+        while run:
             root.update()
-            pg.press('right')
-            # print(f"Осталось {counter}")
 
-        elif (pg.locateOnScreen('res/scsh4.png', region=(987, 552, 267, 146), confidence=.8) 
-              or pg.locateOnScreen('res/scsh5.png', region=(618, 289, 64, 63), confidence=.8) 
-              or pg.locateOnScreen('res/scsh6.png', region=(988, 539, 254, 93), confidence=.8)): 
-            pg.press('left')
-        
-        os.remove('s3.png')
-        os.remove('s.png')
-        root.update()
-    lb['text'] = f'Времени затрачено {int((time.time() - start)/60)} минут и {int((time.time() - start)%60)} секунд,\nвсего {int((time.time() - start))/int(argv[1])} секунд на анкету'
+            pg.screenshot('s3.png', region=(749, 340, 430, 159))
+            pg.screenshot('s.png', region=(1341, 966, 132, 36))
+
+            if counter == 0:
+                run = False
+
+            elif np.equal(np.array(cv2.imread('res/scsh.png', cv2.IMREAD_GRAYSCALE)),
+                          np.array(cv2.imread('s.png', cv2.IMREAD_GRAYSCALE))).all():
+                if np.equal(np.array(cv2.imread('res/scsh3.png', cv2.IMREAD_GRAYSCALE)),
+                            np.array(cv2.imread('s3.png', cv2.IMREAD_GRAYSCALE))).all():
+                    pg.press('esc')
+                counter -= 1
+                lb['text'] = f'{counter}'
+                pg.press('right')
+
+            elif (pg.locateOnScreen('res/scsh4.png', region=(987, 552, 267, 146), confidence=.8)
+                  or pg.locateOnScreen('res/scsh5.png', region=(618, 289, 64, 63), confidence=.8)
+                  or pg.locateOnScreen('res/scsh6.png', region=(988, 539, 254, 93), confidence=.8)):
+                pg.press('left')
+
+            os.remove('s3.png')
+            os.remove('s.png')
+        lb['text'] = (
+            f'Времени затрачено {int((time.time() - start_time) / 60)} минут и '
+            f'{int((time.time() - start_time) % 60)} секунд,'
+            f'\nвсего {int((time.time() - start_time)) / int(argv[1])} секунд на анкету')
+
+    btn = (tk.Button(text='start', command=start))
+    btn.pack()
+
+    lb['text'] = 'Нажмите старт'
 
     root.mainloop()
-    
+
+
 if __name__ == '__main__':
     main()
